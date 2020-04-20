c9efe5f0d69825ca0172d00cd1335cf633456992
InfrastructureAI <InfrastAI@proton.me>
2020-04-20 09:55:17 +0000

    Documentation work.

--- MAE-pytorch/datasets.py
+++ MAE-pytorch/datasets.py
@@ -4,4 +4,13 @@
 # https://github.com/rwightman/pytorch-image-models/tree/master/timm
 # https://github.com/facebookresearch/deit
 # https://github.com/facebookresearch/dino
-# ----------
+# --------------------------------------------------------'
+import os
+import torch
+
+from torchvision import datasets, transforms
+
+from timm.data.constants import \
+    IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
+
+from timm.data import create_transform
