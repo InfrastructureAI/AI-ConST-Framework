a6614998a45758389dcdb35010ab70a6d7196374
InfrastructureAI <InfrastAI@proton.me>
2020-04-20 06:47:58 +0000

    qt3support--

--- MAE-pytorch/datasets.py
+++ MAE-pytorch/datasets.py
@@ -1,1 +1,7 @@
-# -----------------------------------------------
+# --------------------------------------------------------
+# Based on BEiT, timm, DINO and DeiT code bases
+# https://github.com/microsoft/unilm/tree/master/beit
+# https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# https://github.com/facebookresearch/deit
+# https://github.com/facebookresearch/dino
+# ----------
