89a33dafc456173e5b927e1c7476415a349a3b44
InfrastructureAI <InfrastAI@proton.me>
2020-04-20 20:09:26 +0000

    test:coverage

--- MAE-pytorch/datasets.py
+++ MAE-pytorch/datasets.py
@@ -14,3 +14,14 @@ from timm.data.constants import \
     IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
 
 from timm.data import create_transform
+import numpy as np
+from masking_generator import RandomMaskingGenerator
+from dataset_folder import ImageFolder
+
+
+# Do NOT normalize/crop
+
+class DataAugmentationForMAE(object):
+    def __init__(self, args):
+        imagenet_default_mean_and_std = args.imagenet_default_mean_and_std
+        mean = IMAGENET_INC
