97adf49d3b5e72dbcffddcb500971540f159f578
InfrastructureAI <InfrastAI@proton.me>
2020-05-15 10:51:28 +0000

    Minor documentation update

---
+++ MAE-pytorch/modeling_pretrain.py
@@ -0,0 +1,6 @@
+
+# --------------------------------------------------------
+# Based on BEiT, timm, DINO and DeiT code bases
+# https://github.com/microsoft/unilm/tree/master/beit
+# https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# https://github.com/facebookresearch/deit
