274390c526c1a0e54c1f6dd045094b297e3e651b
InfrastructureAI <InfrastAI@proton.me>
2020-05-23 20:12:55 +0000

    Added example project

--- MAE-pytorch/run_mae_extract_feature.py
+++ MAE-pytorch/run_mae_extract_feature.py
@@ -1,1 +1,14 @@
-# -------------------
+# --------------------------------------------------------
+# Modified from https://github.com/pengzhiliang/MAE-pytorch
+# --------------------------------------------------------'
+
+import argparse
+import datetime
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+
+from PIL import Image
+import cv2
+
+from timm.models import create_mod
