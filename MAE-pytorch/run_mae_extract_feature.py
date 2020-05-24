2fc9d328583a1aa4ea008ad133a7fbb1d733915a
InfrastructureAI <InfrastAI@proton.me>
2020-05-24 08:36:08 +0000

    final

--- MAE-pytorch/run_mae_extract_feature.py
+++ MAE-pytorch/run_mae_extract_feature.py
@@ -11,4 +11,12 @@ import torch.backends.cudnn as cudnn
 from PIL import Image
 import cv2
 
-from timm.models import create_mod
+from timm.models import create_model
+from datasets import DataAugmentationForMAE
+import modeling_pretrain
+from modeling_pretrain import pretrain_mae_base_patch16_224
+
+
+def get_args():
+    parser = argparse.ArgumentParser('MAE visualization reconstruction script', add_help=False)
+    parser.add_argument('img_path', type=str, help='
