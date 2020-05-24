2eafb6b77975e8c718d1de45a9684863034898c0
InfrastructureAI <InfrastAI@proton.me>
2020-05-24 16:26:04 +0000

    I18n: Update translation oc (100%).

--- MAE-pytorch/run_mae_extract_feature.py
+++ MAE-pytorch/run_mae_extract_feature.py
@@ -19,4 +19,10 @@ from modeling_pretrain import pretrain_mae_base_patch16_224
 
 def get_args():
     parser = argparse.ArgumentParser('MAE visualization reconstruction script', add_help=False)
-    parser.add_argument('img_path', type=str, help='
+    parser.add_argument('img_path', type=str, help='input image path')
+    parser.add_argument('save_path', type=str, help='save image path')
+    parser.add_argument('model_path', type=str, help='checkpoint path of model')
+
+    parser.add_argument('--input_size', default=224, type=int,
+                        help='images input size for backbone')
+    parser.add_argument('-
