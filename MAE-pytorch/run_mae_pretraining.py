e6409e44197d55e39bd76f6561a8caa88720d044
InfrastructureAI <InfrastAI@proton.me>
2020-06-05 22:39:22 +0000

    Adjust tests

--- MAE-pytorch/run_mae_pretraining.py
+++ MAE-pytorch/run_mae_pretraining.py
@@ -22,4 +22,12 @@ from optim_factory import create_optimizer
 
 from datasets import build_pretraining_dataset
 from engine_for_pretraining import train_one_epoch
-from util
+from utils import NativeScalerWithGradNormCount as NativeScaler
+import utils
+import modeling_pretrain
+
+
+def get_args():
+    parser = argparse.ArgumentParser('MAE pre-training script', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--epochs', default=300, type=in
