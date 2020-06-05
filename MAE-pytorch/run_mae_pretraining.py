0a289c3c0a49d28c0838b98b26bb334e8c9fadfd
InfrastructureAI <InfrastAI@proton.me>
2020-06-05 15:16:48 +0000

    Update ADV.md

--- MAE-pytorch/run_mae_pretraining.py
+++ MAE-pytorch/run_mae_pretraining.py
@@ -8,4 +8,18 @@
 
 import argparse
 import datetime
-import nump
+import numpy as np
+import time
+import torch
+import torch.backends.cudnn as cudnn
+import json
+import os
+
+from pathlib import Path
+
+from timm.models import create_model
+from optim_factory import create_optimizer
+
+from datasets import build_pretraining_dataset
+from engine_for_pretraining import train_one_epoch
+from util
