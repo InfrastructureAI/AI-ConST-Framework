976e20a9e63c4dba69154e2478eb2f4d1cfb41b7
InfrastructureAI <InfrastAI@proton.me>
2020-08-27 11:00:06 +0000

    Minor formatting improvements

--- MAE-pytorch/run_mae_vis.py
+++ MAE-pytorch/run_mae_vis.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/11/18 22:40
 # @Author  : zhao pengfei
-# @Email   : zsonghuan@gma
+# @Email   : zsonghuan@gmail.com
+# @File    : run_mae_vis.py
+# --------------------------------------------------------
+# Based on BEiT, timm, DINO and DeiT code bases
+# https://github.com/microsoft/unilm/tree/master/beit
+# https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# https://github.com/facebookresea
