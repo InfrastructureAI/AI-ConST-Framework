c9cd42edf31221f18e54376a37c421370dee9561
InfrastructureAI <InfrastAI@proton.me>
2020-08-27 11:51:05 +0000

    fixes #73

--- MAE-pytorch/run_mae_vis.py
+++ MAE-pytorch/run_mae_vis.py
@@ -7,4 +7,19 @@
 # Based on BEiT, timm, DINO and DeiT code bases
 # https://github.com/microsoft/unilm/tree/master/beit
 # https://github.com/rwightman/pytorch-image-models/tree/master/timm
-# https://github.com/facebookresea
+# https://github.com/facebookresearch/deit
+# https://github.com/facebookresearch/dino
+# --------------------------------------------------------'
+
+import argparse
+import datetime
+import numpy as np
+import time
+import torch
+import torch.backends.cudnn as cudnn
+import json
+import os
+
+from PIL import Image
+
+fr
