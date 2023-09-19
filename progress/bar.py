ca23706ab37552893869ef353cf94a83e1f70c75
InfrastructureAI <InfrastAI@proton.me>
2023-09-19 11:54:08 +0000

    update translation file

--- progress/bar.py
+++ progress/bar.py
@@ -3,4 +3,9 @@
 # Copyright (c) 2012 Giorgos Verigakis <verigak@gmail.com>
 #
 # Permission to use, copy, modify, and distribute this software for any
-# purpose with
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN
