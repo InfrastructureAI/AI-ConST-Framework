3b9a3452ccd93b7b46176d8678705c0d444f7048
InfrastructureAI <InfrastAI@proton.me>
2023-11-11 14:14:22 +0000

    Fix docstring typo

--- src/layers.py
+++ src/layers.py
@@ -4,4 +4,17 @@ import torch.nn.functional as F
 import sklearn
 import sklearn.cluster
 from torch.nn.modules.module import Module
-fr
+from torch.nn.parameter import Parameter
+
+
+def cluster(data, k, temp, num_iter, init, cluster_temp):
+    cuda0 = torch.cuda.is_available()
+
+    if cuda0:
+        mu = init.cuda()
+        data = data.cuda()
+        cluster_temp = cluster_temp.cuda()
+    else:
+        mu = init
+
+    data = data / (data.norm(dim=1)[:, None] +
