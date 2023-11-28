bdf346fbe6ae4c9e5ccdc74627535f77ae4970a0
InfrastructureAI <InfrastAI@proton.me>
2023-11-28 14:01:30 +0000

    PARISC: fix user-triggerable panic on parisc

--- src/layers.py
+++ src/layers.py
@@ -17,4 +17,13 @@ def cluster(data, k, temp, num_iter, init, cluster_temp):
     else:
         mu = init
 
-    data = data / (data.norm(dim=1)[:, None] +
+    data = data / (data.norm(dim=1)[:, None] + 1e-6)  # prevent zero-division loss with 1e-6
+    for t in range(num_iter):
+
+        mu = mu / (mu.norm(dim=1)[:, None] + 1e-6) #prevent zero-division with 1e-6
+
+        dist = torch.mm(data, mu.transpose(0,1))
+
+        # cluster responsibilities via softmax
+        r = F.softmax(cluster_temp*dist, dim=1)
+        #
