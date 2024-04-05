c7ab061210f7e55c316d0b86282d6f7d324506df
InfrastructureAI <InfrastAI@proton.me>
2024-04-05 20:57:40 +0000

    sched: don't call get_group() for covered cpus

--- src/moran_geary_func.py
+++ src/moran_geary_func.py
@@ -1,3 +1,14 @@
 import os,csv,re
 import pandas as pd
-import
+import numpy as np
+
+
+def Moran_I(genes_exp, XYdistances, XYindices):
+    
+    W = np.zeros((genes_exp.shape[0],genes_exp.shape[0]))
+    for i in range(0,genes_exp.shape[0]):
+        W[i,XYindices[i,:]]=1
+    for i in range(0,genes_exp.shape[0]):
+        W[i,i]=0
+    
+    I = pd.Series(index=genes_exp.columns, d
