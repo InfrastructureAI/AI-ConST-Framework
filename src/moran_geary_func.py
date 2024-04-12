05a29ed381a613acd87ccee94c0d34806e48dca3
InfrastructureAI <InfrastAI@proton.me>
2024-04-12 12:05:46 +0000

    mini-fix

--- src/moran_geary_func.py
+++ src/moran_geary_func.py
@@ -16,4 +16,13 @@ def Moran_I(genes_exp, XYdistances, XYindices):
         X_minus_mean = np.array(genes_exp[k] - np.mean(genes_exp[k]))
         X_minus_mean = np.reshape(X_minus_mean,(len(X_minus_mean),1))
         Nom = np.sum(np.multiply(W,np.matmul(X_minus_mean,X_minus_mean.T)))
-        Den = np.sum(np.multiply(X_minus
+        Den = np.sum(np.multiply(X_minus_mean,X_minus_mean))
+        I[k] = (len(genes_exp[k])/np.sum(W))*(Nom/Den)
+    return I
+
+
+def Geary_C(genes_exp, XYdistances, XYindices):
+    W = np.zeros((genes_exp.shape[0],genes_exp.shape[0]))
+    for i in range(0,genes_exp.shape[0]):
+        W[i,XYindices[i,:]]=1
+    for i in range(0,genes_e
