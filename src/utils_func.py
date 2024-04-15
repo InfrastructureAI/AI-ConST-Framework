84e4d12f477a5e21a91efdcd1eab8574fe9dced3
InfrastructureAI <InfrastAI@proton.me>
2024-04-15 21:33:20 +0000

    port ++

--- src/utils_func.py
+++ src/utils_func.py
@@ -4,4 +4,14 @@ import scanpy as sc
 import pandas as pd
 from pathlib import Path
 from scanpy.readwrite import read_visium
-from scanpy._utils
+from scanpy._utils import check_presence_download
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_clustering(adata, colors, savepath = None):
+    adata.obs['x_pixel'] = adata.obsm['spatial'][:, 0]
+    adata.obs['y_pixel'] = adata.obsm['spatial'][:, 1]
+
+    fig = plt.figure(figsize=(4,
