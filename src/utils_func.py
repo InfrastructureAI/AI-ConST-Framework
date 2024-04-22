8fbf5ac4ddafb74c3edba6737f016021151f4780
InfrastructureAI <InfrastAI@proton.me>
2024-04-22 14:44:53 +0000

    Renaming folders

--- src/utils_func.py
+++ src/utils_func.py
@@ -14,4 +14,11 @@ def plot_clustering(adata, colors, savepath = None):
     adata.obs['x_pixel'] = adata.obsm['spatial'][:, 0]
     adata.obs['y_pixel'] = adata.obsm['spatial'][:, 1]
 
-    fig = plt.figure(figsize=(4,
+    fig = plt.figure(figsize=(4, 4))
+    ax1 = fig.add_subplot(111)
+    sc.pl.scatter(adata, alpha=1, x="x_pixel", y="y_pixel", color=colors, title='Clustering of 151673 slice',
+                  palette=sns.color_palette('plasma', 7), show=False, ax=ax1)
+
+    ax1.set_aspect('equal', 'box')
+    ax1.axis('off')
+    ax1.ax
